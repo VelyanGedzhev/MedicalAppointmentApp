2dfa4f1bf440f654e4ee7ccb8bae0a1581969646
Velyan Gedzhev <v.gedzhev@gmail.com>
2022-04-13 14:29:46 +0300

    Add base skeleton

---
+++ MedicalAppointment/MedicalAppointment.Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MedicalAppointment.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalAppointment.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
